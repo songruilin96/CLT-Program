c182ba603aba6922a9c0ddaf0db27802a4af5f1f
songr <songr@DESKTOP-51SP2HR>
2019-07-21 15:28:16 +0800

    Assignment

--- src/assignment/Problem06.java
+++ src/assignment/Problem06.java
@@ -4,8 +4,18 @@ public class Problem06 {
 
 	public static void main(String[] args) {
 		
-		
-
-	}
-
+		 int rows, col, k=3;     
+		 
+		 System.out.println("*");
+		 
+		 for(rows=1; rows<=4; rows++) {                //rows
+			 
+			 for(col=1; col<k; col++){              //columns
+				 System.out.print("* ");          
+				 }
+			 
+			 k = k + 2;   // each line add 2 (even)
+			 System.out.println(); 
+			 }
+		 }	
 }
